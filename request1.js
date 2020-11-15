1c75400f85af3c54a924a753fa3259b568a03611
asilter <asilter79@hotmail.com>
2020-11-15 12:33:18 +0300

    get web request results and write to a file after response end

--- request1.js
+++ request1.js
@@ -1,13 +1,52 @@
-var request = require('request');
+//const https = require('http')
+const https = require('https')
 
-request({
+const options = {
+    //hostname: 'example.com',
+    hostname: 'www.tripadvisor.com',
+    //port: 80,
+    port: 443,
+    //path: '/',
+    path: '/Restaurants-g186338-London_England.html',
+    method: 'GET',
+    headers: {
+        'Content-Type': 'text/html'
+    }
+};
 
-    uri: "http://example.com",
-
-}, function (error, response, body) {
-    
-    console.log(body);
-
-
-    
+var totalData = "";
+const req = https.request(options, res => {
+    console.log("statusCode:" + res.statusCode);
+    res.on('data', d => {
+        totalData = totalData + d;
+        //console.log(totalData.length);
+        //console.log("data (d) : " + d);
+    });
+    res.on('end', () => {
+        console.log(totalData.length);
+        const fs = require('fs');
+        
+        fs.writeFile("/tmp/tripadvisor.html", totalData, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+        
+        // Or
+        //fs.writeFileSync('/tmp/tripadvisor.txt', totalData);
+    })
 });
+
+req.on('error', error => {
+    console.log("Failed!");
+    console.error(error);
+});
+
+
+
+req.end();
+
+
+
+
